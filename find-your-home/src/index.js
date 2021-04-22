7ea36bcb84a3445755c3c1f1c184d7a22f202518
Brielleowl <luoxiaofo619@gmail.com>
2021-04-22 00:58:47 -0500

    initialize project

---
+++ find-your-home/src/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import './index.css';
+// import 'semantic-ui-css/semantic.min.css'
+// import 'semantic-ui-css/semantic.js';
+// import 'semantic-ui';
+
+import '../semantic/dist/semantic.css';
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+);
